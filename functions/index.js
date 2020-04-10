dc98b539ecdd8fbffb6ae0306b44b66f0e9efc94
hbrooks3 <hunterbrooks@outlook.com>
2020-04-10 16:57:49 -0500

    added firebase hosting, functions, and firestore

---
+++ functions/index.js
@@ -0,0 +1,10 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+exports.ssr = functions.https.onRequest((_req, res) => res.send('hello from function'));

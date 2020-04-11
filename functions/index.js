f8c95295e550ad70f59c1d5cbb37a613a83928e3
hbrooks3 <hunterbrooks@outlook.com>
2020-04-11 12:45:05 -0500

    added new workout function

--- functions/index.js
+++ functions/index.js
@@ -12,3 +12,56 @@ exports.ssr = functions.https.onRequest((_req, res) => res.send('hello from func
 exports.test = functions.https.onRequest((_req, res) => {
     res.redirect("https://bodyweight-blitz.web.app/");
 });
+
+let blank_workout = [
+    {
+        name: "push-ups",
+        time: 0,
+        reps: [],
+        goal: {
+            rep: {
+                best: 0,
+                last: 0,
+            },
+            time: {
+                best: 0,
+                last: 0,
+            },
+        },
+    },
+    {
+        name: "squats",
+        time: 0,
+        reps: [],
+        goal: {
+            rep: {
+                best: 0,
+                last: 0,
+            },
+            time: {
+                best: 0,
+                last: 0,
+            },
+        },
+    },
+    {
+        name: "sit-ups",
+        time: 0,
+        reps: [],
+        goal: {
+            rep: {
+                best: 0,
+                last: 0,
+            },
+            time: {
+                best: 0,
+                last: 0,
+            },
+        },
+    },
+]
+
+exports.new = functions.https.onRequest((_req, res) => {
+    res.json({workout: blank_workout})
+    // res.send()
+})
